d045a8e0812fa385b913204979b5b9ea71e2b138
Oliver <felix.beer@students.hs-mainz.de>
2022-11-10 20:25:31 +0100

    BONUS-
    Sound and Destroy

--- Assets/Scripts/Decoy/TriggerDecoy.cs
+++ Assets/Scripts/Decoy/TriggerDecoy.cs
@@ -7,12 +7,23 @@ public class TriggerDecoy : MonoBehaviour
 
     public GameObject Roboter;
     // Start is called before the first frame update
+
+    AudioSource audioData;
+
+
     private void OnTriggerEnter(Collider other){
         Debug.Log(gameObject.name + " touched this " + other.name + " object");
 
-        
         if(other.name == Roboter.name){
             Debug.Log("Decoy eingesammelt");
+
+            audioData = GetComponent<AudioSource>();
+            audioData.Play(0);
+            Debug.Log("started");
+
+            Destroy(gameObject, 1);
+
+            
         }
         
     }
--- Assets/Scripts/Decoy/TriggerGoal.cs
+++ Assets/Scripts/Decoy/TriggerGoal.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class TriggerGoal : MonoBehaviour
 {
     public GameObject Roboter;
+
+    AudioSource audioData;
+
     private void OnTriggerEnter(Collider other){
         Debug.Log(gameObject.name + " hat das Ziel: " + other.name + "erreicht");
 
         if(other.name == Roboter.name){
             Debug.Log("Victory");
+            
+
+            audioData = GetComponent<AudioSource>();
+            audioData.Play(0);
+            Debug.Log("started");
         }
     }
 }
