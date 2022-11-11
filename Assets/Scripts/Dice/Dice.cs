580f18783384eec2935f900e2e5d95384f671d07
Oliver <felix.beer@students.hs-mainz.de>
2022-11-11 13:34:48 +0100

    Dice For

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -13,6 +13,9 @@ public class Dice : MonoBehaviour
 
     public string youWinText = ". Das bedeutet du hast gewonnen :D";
 
+
+    int[] winningNumbers = new int[3];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,26 +27,37 @@ public class Dice : MonoBehaviour
     {
         if (Input.GetKeyDown("d")) {
 
-            int[] winningNumbers = new int[3];
+            
 
             winningNumbers [0] = Random.Range(1, 7);
-            winningNumbers [1] = Random.Range(1, 7);
-            while (winningNumbers[0] == winningNumbers [1]){
+            // winningNumbers [1] = Random.Range(1, 7);
+            do{
                 winningNumbers [1] = Random.Range(1, 7);
-            }
-            winningNumbers [2] = Random.Range(1, 7);
-            while (winningNumbers[0] == winningNumbers [2] || winningNumbers [1] == winningNumbers[2]){
+            }while (winningNumbers[0] == winningNumbers [1]);
+
+            // winningNumbers [2] = Random.Range(1, 7);
+            do{
                 winningNumbers [2] = Random.Range(1, 7);
-            }
+            }while (winningNumbers[0] == winningNumbers [2] || winningNumbers [1] == winningNumbers[2]);
 
             diceNumber = Random.Range(1, 7);
             Debug.Log("Du hast eine: " + diceNumber + " gewürfelt!");
 
             
-            if (winningNumbers[0] == diceNumber || winningNumbers[1] == diceNumber || winningNumbers[2] == diceNumber) {
-                Debug.Log(winNumberText + winningNumbers[0] +", " + winningNumbers[1] + ", " + winningNumbers[2] + youWinText);
-            }else {
-                Debug.Log(winNumberText + winningNumbers [0] + ", " + winningNumbers[1]+ ", " + winningNumbers[2] + ". Das bedeutet du hast verloren :C");
+            // if (winningNumbers[0] == diceNumber || winningNumbers[1] == diceNumber || winningNumbers[2] == diceNumber) {
+            //     Debug.Log(winNumberText + winningNumbers[0] +", " + winningNumbers[1] + ", " + winningNumbers[2] + youWinText);
+            // }else {
+            //     Debug.Log(winNumberText + winningNumbers [0] + ", " + winningNumbers[1]+ ", " + winningNumbers[2] + ". Das bedeutet du hast verloren :C");
+            // }
+
+
+            for (int i = 0; i < winningNumbers.Length; i++){
+                // Debug.Log(i);
+                if (winningNumbers[i] == diceNumber){
+                    Debug.Log(winNumberText + winningNumbers[0] +", " + winningNumbers[1] + ", " + winningNumbers[2] + youWinText);
+                }else if (i == winningNumbers.Length){
+                    Debug.Log(winNumberText + winningNumbers [0] + ", " + winningNumbers[1]+ ", " + winningNumbers[2] + ". Das bedeutet du hast verloren :C");
+                }
             }
         }
 
